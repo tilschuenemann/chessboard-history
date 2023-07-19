1cb63119d268b85077b7a32018379af4190d96b5
Leon Kie <leon.kiesgen@web.de>
2023-07-19 19:13:40 +0200

    adding exponential fade type

--- src/chessboard_history/chessboard_history.py
+++ src/chessboard_history/chessboard_history.py
@@ -11,7 +11,7 @@ from typing import List
 class ChessboardHistory:
     def __init__(self) -> None:
         pass
-
+    
     def _hex_wrapper(self, input: List[int]) -> List[str]:
         """Transform a list of ints in range 0-255 to the last two digits of their hex representation."""
         l = [f"{x:#0{4}x}" for x in input]
@@ -25,8 +25,7 @@ class ChessboardHistory:
         Args:
             len: length of input to match
         """
-        l = np.linspace(1, 255, len).tolist()
-        l = [int(x) for x in l]
+        l = np.int16(np.linspace(1, 255, len)).tolist()
         return self._hex_wrapper(l)
 
     def _gen_last_n(self, len: int, n: int) -> List[str]:
@@ -38,7 +37,22 @@ class ChessboardHistory:
         """
         l = [64 for _ in range(len - n)]
         l = l + [192 for _ in range(n)]
+
+
         return self._hex_wrapper(l)
+    
+    def _gen_exp(self, len: int, base: int = 10) -> List[str]:
+        """Creates a list of opacities that range from 0-255 in hex format.
+        The value increases exponentially until 255 (in hex).
+
+        Args:
+            len: length of input to match
+            base: ]1;inf] determines the growth rate of the exp function.
+        """
+        l=list(np.int16(np.power(base,np.linspace(-1,0, len))*255))
+        
+        return self._hex_wrapper(l)
+
 
     def print_history(self, pgn: str, rgb: str, fade_function: str = "evenly") -> None:
         """
@@ -47,7 +61,7 @@ class ChessboardHistory:
             rgb:
             fade_function: "evenly", "last2"
         """
-        if fade_function not in ["evenly", "last2"]:
+        if fade_function not in ["evenly", "last2","exp"]:
             raise Exception("Please provide a proper FADE_FUNCTION!")
 
         hex_rgb_pat = r"^#(?:[0-9a-fA-F]{3}){1,2}$"
@@ -68,6 +82,8 @@ class ChessboardHistory:
         move_count = len(arrows)
         if fade_function == "evenly":
             opacities = self._gen_evenly(move_count)
+        elif fade_function == "exp": 
+            opacities = self._gen_exp(move_count)
         else:
             opacities = self._gen_last_n(move_count, 2)
 
@@ -83,4 +99,4 @@ class ChessboardHistory:
 if __name__ == "__main__":
     pgn = "1. e4 e5 2. Qf3 Nc6 3. Bc4 Nf6 4. Ne2 Bc5 5. a3 d6 6. O-O Bg4 7. Qd3 Nh5 8. h3 Bxe2 9. Qxe2 Nf4 10. Qe1 Nd4 11. Bb3 Nxh3+ 12. Kh2 Qh4 13. g3 Nf3+ 14. Kg2 Nxe1+ 15. Rxe1 Qg4 16. d3 Bxf2 17. Rh1 Qxg3+ 18. Kf1 Bd4 19. Ke2 Qg2+ 20. Kd1 Qxh1+ 21. Kd2 Qg2+ 22. Ke1 Ng1 23. Nc3 Bxc3+ 24. bxc3 Qe2#"
     ch = ChessboardHistory()
-    ch.print_history(pgn, "#ff0000", fade_function="evenly")
+    ch.print_history(pgn, "#ff0000", fade_function="exp")
