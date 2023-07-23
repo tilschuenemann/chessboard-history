be959bf7f5f495901c7cef9711d96fdc045de02b
Til Schünemann <til.schuenemann@mailbox.org>
2023-07-23 09:12:50 +0200

    chore: update pyproject and linting

--- lint.sh
+++ lint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-poetry run black src/chessboard_history/
-poetry run isort
+poetry run black .
+poetry run isort .
 poetry run bandit -r src/chessboard_history
 poetry run safety check
 poetry run mypy src/chessboard_history
+poetry run coverage run -m pytest
+poetry run coverage report
